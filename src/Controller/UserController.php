bbd6e4f08c9dea5adeef49bffc05ead9661f3d5d
nayalNanou <nayal.christophcolomb@gmail.com>
2021-01-14 14:34:28 +0100

    My Fist Commit

---
+++ src/Controller/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/users", name="user_")
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('users/index.html.twig', [
+        ]);
+    }
+}
+
